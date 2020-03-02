8ea9fdfc491f4e62a786ebaaf58182d0f0d4d9ef
reecedoza <mauro.doza@gmail.com>
2020-03-01 20:40:04 -0500

    Snap to center on click

--- src/control.js
+++ src/control.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-lonely-if */
 /* eslint-disable guard-for-in */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable import/prefer-default-export */
@@ -12,23 +13,57 @@ let camera;
 let controls;
 let renderer;
 let scene;
-const models = {};
+const models = [];
 let topLight;
+let group;
+let selectedObject;
+const savedPositions = {};
+const tweens = {};
 
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
 const aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-const frustumSize = 20;
+const frustumSize = 10;
 
 const xMin = (frustumSize * aspect) / -2;
 const xMax = (frustumSize * aspect) / 2;
 const zMax = (frustumSize / 2);
 const zMin = (frustumSize / -2);
 
-function getRandomPositions(min, max) {
+function getRandomPositions() {
+  // return [{x,z}] or {x:[],z:[]}
+  const positions = { x: [], z: [] };
+  for (let i = 0; i < 100; i++) {
+    if (Math.random() >= 0.5) {
+      if (Math.random() >= 0.5) {
+        positions.x.push(getRandomInt(xMin, xMax));
+        positions.z.push(zMin);
+      } else {
+        positions.x.push(getRandomInt(xMin, xMax));
+        positions.z.push(zMax);
+      }
+    } else {
+      if (Math.random() >= 0.5) {
+        positions.x.push(xMin);
+        positions.z.push(getRandomInt(zMin, zMax));
+      } else {
+        positions.x.push(xMax);
+        positions.z.push(getRandomInt(zMin, zMax));
+      }
+    }
+  }
+
+  return positions;
+}
+
+function getRandomYPositions() {
   const positions = [];
   for (let i = 0; i < 100; i++) {
-    positions.push(getRandomInt(min, max));
+    if (Math.random() >= 0.5) {
+      positions.push(getRandomInt(-10, -5));
+    } else {
+      positions.push(getRandomInt(5, 20));
+    }
   }
   return positions;
 }
@@ -50,18 +85,20 @@ export function start() {
   );
 
   // new THREE.PerspectiveCamera(
-  //   45,
+  //   75,
   //   aspect,
   //   0.1,
   //   1000,
   // );
 
-  camera.position.y = 15;
+  camera.position.y = 30;
 
   const axesHelper = new THREE.AxesHelper(5);
   scene.add(axesHelper);
+  camera.lookAt( scene.position )
+  // createLights();
 
-  createLights();
+  group = new THREE.Group();
   loadModels();
 
   renderer = new THREE.WebGLRenderer({ alpha: 1 });
@@ -70,21 +107,51 @@ export function start() {
 
   container = document.querySelector('#container');
   container.appendChild(renderer.domElement);
+  container.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const intersects = getIntersects(event.layerX, event.layerY);
+    if (intersects.length > 0) {
+      const res = intersects.filter((res) => res && res.object)[0];
+
+      if (res && res.object) {
+        selectedObject = res.object;
+
+        TWEEN.remove(tweens[selectedObject.name]);
+        selectedObject.position.x = savedPositions[selectedObject.name].x;
+        selectedObject.position.z = savedPositions[selectedObject.name].z;
+      }
+    }
+  });
 
   controls = new OrbitControls(camera, container);
+
 
   window.addEventListener('resize', onWindowResize);
 
   animate();
 }
 
-function animate() {
+const raycaster = new THREE.Raycaster();
+const mouseVector = new THREE.Vector3();
+
+function getIntersects(x, y) {
+  x = (x / window.innerWidth) * 2 - 1;
+  y = -(y / window.innerHeight) * 2 + 1;
+
+  mouseVector.set(x, y, 0.5);
+  raycaster.setFromCamera(mouseVector, camera);
+
+  return raycaster.intersectObject(group, true);
+}
+
+function animate(time) {
   requestAnimationFrame(animate);
 
   // for (const item in models) {
   //   runModel(models[item]);
   // }
-  TWEEN.update();
+  TWEEN.update(time);
 
   renderer.render(scene, camera);
 }
@@ -109,23 +176,25 @@ function loadModels() {
     // console.log(dumpObject(gltf.scene).join('\n'));
     if (gltf.scene) {
       for (const item of gltf.scene.children) {
+        savedPositions[item.name] = { x: item.position.x, z: item.position.z };
         // item.castShadow = true;
         item.position.x = getRandomInt(xMin, xMax);
-        item.position.z = getRandomInt(zMax, zMin);
-        models[item.name] = item;
+        item.position.z = getRandomInt(zMin, zMax);
+        models.push(item);
       }
     }
-    for (const item in models) {
-      scene.add(models[item]);
+    for (const item of models) {
+      group.add(item);
 
-      new TWEEN.Tween(models[item].position)
-        .to({
-          x: getRandomPositions(xMin, xMax),
-          z: getRandomPositions(zMax, zMin),
-        }, 100000)
+      const tween = new TWEEN.Tween(item.position)
+        .to(getRandomPositions(), 300000)
         .repeat(Infinity)
         .start();
+
+      tweens[item.name] = tween;
     }
+
+    scene.add(group);
   };
   const onProgress = (xhr) => {
     console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`);
