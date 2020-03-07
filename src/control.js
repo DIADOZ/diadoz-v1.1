e12fbbc767b9ab9b0150153dc64446e5d0263833
reecedoza <mauro.doza@gmail.com>
2020-03-07 16:19:49 -0500

    Transition to type then fade, clean up

--- src/control.js
+++ src/control.js
@@ -1,8 +1,3 @@
-/* eslint-disable no-lonely-if */
-/* eslint-disable guard-for-in */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-use-before-define */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
@@ -11,15 +6,11 @@ import { fadeIn } from './index';
 
 let container;
 let camera;
-let controls;
 let renderer;
 let scene;
 const models = [];
-let topLight;
 let modelGroup;
-let selectedObject;
 let savedPositions = {};
-const tweens = {};
 
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
@@ -28,21 +19,13 @@ const frustumSize = 10;
 
 const xMin = (frustumSize * aspect) / -2;
 const xMax = (frustumSize * aspect) / 2;
-const zMax = (frustumSize / 2);
-const zMin = (frustumSize / -2);
-
+const zMax = frustumSize / 2;
+const zMin = frustumSize / -2;
 
 export function start() {
   scene = new THREE.Scene();
 
-  camera = new THREE.OrthographicCamera(
-    xMin,
-    xMax,
-    zMax,
-    zMin,
-    0.1,
-    1000,
-  );
+  camera = new THREE.OrthographicCamera(xMin, xMax, zMax, zMin, 0.1, 1000);
 
   // new THREE.PerspectiveCamera(
   //   75,
@@ -52,15 +35,7 @@ export function start() {
   // );
 
   camera.position.y = 30;
-
-  // const axesHelper = new THREE.AxesHelper(5);
-  // scene.add(axesHelper);
   camera.lookAt(scene.position);
-  
-  // createLights();
-
-  modelGroup = new THREE.Group();
-  loadModels();
 
   renderer = new THREE.WebGLRenderer({ alpha: 1 });
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -70,20 +45,19 @@ export function start() {
   container.appendChild(renderer.domElement);
   container.addEventListener('click', snapModel);
 
-  controls = new OrbitControls(camera, container);
+  modelGroup = new THREE.Group();
+  loadModels();
 
   window.addEventListener('resize', onWindowResize);
 
   animate();
 }
 
-
-
 function loadModels() {
   const loader = new GLTFLoader();
 
   // A reusable function to set up the models. Position parameter to move model
-  const onLoad = (gltf) => {
+  const onLoad = gltf => {
     if (gltf.scene) {
       for (let i = gltf.scene.children.length - 1; i >= 0; i--) {
         const model = gltf.scene.children[i];
@@ -92,75 +66,63 @@ function loadModels() {
 
         modelGroup.add(model);
 
-        const tween1 = new TWEEN.Tween(model.position)
-          .to(getRandomPositions(), 300000)
-          .repeat(Infinity)
-          .onStop(transitionModel)
-          .start();
-
-        // const tween2 = new TWEEN.Tween(model.position)
-        //   .to(getLockedPositions(), 300000)
-        //   .onStop(transitionPage);
-
         models.push({
           model,
-          savedPositions,
-          tween1,
-          // tween2,
+          savedPositions
         });
-        tweens[model.name] = tween1;
       }
     }
-
     scene.add(modelGroup);
+    createTweens();
+    startTween('tween1');
   };
-  const onProgress = (xhr) => {
+  const onProgress = xhr => {
     console.log(`${(xhr.loaded / xhr.total) * 100}% loaded`);
   };
-  const onError = (errorMessage) => {
+  const onError = errorMessage => {
     console.log(errorMessage);
   };
 
   // model is loaded asynchronously,
-  // const atlasPosition = new THREE.Vector3(0.02, 0, 0);
   loader.load(
     './assets/model/diadoz-official-blender.glb',
-    (gltf) => onLoad(gltf),
+    gltf => onLoad(gltf),
     onProgress,
-    onError,
+    onError
   );
+}
+
+function createTweens() {
+  models.forEach(modelObj => {
+    const tween1 = new TWEEN.Tween(modelObj.model.position)
+      .to(getRandomPositions(), 400000)
+      .repeat(Infinity)
+      .onStop(transitionModel);
+
+    const tween2 = new TWEEN.Tween(modelObj.model.position)
+      .to(getLockedPositions(modelObj.savedPositions), 1000)
+      .onComplete(transitionPage);
+
+    modelObj.tween1 = tween1;
+    modelObj.tween2 = tween2;
+  });
+}
+
+function startTween(tweenType) {
+  models.forEach(modelObj => {
+    modelObj[tweenType].start();
+  });
 }
 
 function transitionModel() {
   if (TWEEN.getAll().length === 0) {
-    for (const modelObj of models) {
-      modelObj.tween2.start();
-    }
+    startTween('tween2');
   }
 }
 
 function transitionPage() {
-  // call fade in
-}
-
-function animate(time) {
-  requestAnimationFrame(animate);
-
-  TWEEN.update(time);
-
-  renderer.render(scene, camera);
-}
-
-function createLights() {
-  const ambientLight = new THREE.HemisphereLight(0xfffff0, 5);
-
-  topLight = new THREE.PointLight(0xcd712c, 5, 10, 2);
-  topLight.castShadow = false;
-  topLight.position.x = 0;
-  topLight.position.y = 10;
-  topLight.position.z = 0;
-
-  scene.add(ambientLight, topLight);
+  new OrbitControls(camera, container);
+  fadeIn();
 }
 
 function snapModel(event) {
@@ -168,14 +130,14 @@ function snapModel(event) {
 
   const intersects = getIntersects(event.layerX, event.layerY);
   if (intersects.length > 0) {
-    const res = intersects.filter((res) => res && res.object)[0];
+    const res = intersects.filter(res => res && res.object)[0];
 
     if (res && res.object) {
-      const modelObj = models.find((obj) => obj.model.name === res.object.name);
+      const modelObj = models.find(obj => obj.model.name === res.object.name);
 
-      modelObj.tween.stop();
       modelObj.model.position.x = modelObj.savedPositions.x;
       modelObj.model.position.z = modelObj.savedPositions.z;
+      modelObj.tween1.stop();
     }
   }
 }
@@ -193,55 +155,55 @@ function getIntersects(x, y) {
   return raycaster.intersectObject(modelGroup, true);
 }
 
+function animate(time) {
+  requestAnimationFrame(animate);
+
+  TWEEN.update(time);
+
+  renderer.render(scene, camera);
+}
+
 function onWindowResize() {
   camera.aspect = aspect;
-
-  // update the camera's frustum
-  // camera.updateProjectionMatrix();
 
   renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
-function getLockedPositions() {
-  // take saved positions and return same except lower down
-  const positions = { x: [], z: [] };
+function getLockedPositions(centerPosition) {
+  const position = {
+    z: centerPosition.z + 1
+  };
+
+  return position;
 }
 
 function getRandomPositions() {
   // return [{x,z}] or {x:[],z:[]}
   const positions = { x: [], z: [] };
+  const xMaxAdjusted = xMax - 0.7;
+  const xMinAdjusted = xMin + 0.7;
+  const zMaxAdjusted = zMax - 0.5;
+  const zMinAdjusted = zMin + 0.5;
   for (let i = 0; i < 100; i++) {
     if (Math.random() >= 0.5) {
       if (Math.random() >= 0.5) {
-        positions.x.push(getRandomInt(xMin, xMax));
-        positions.z.push(zMin + 0.5);
+        positions.x.push(getRandomInt(xMinAdjusted, xMaxAdjusted));
+        positions.z.push(zMinAdjusted);
       } else {
-        positions.x.push(getRandomInt(xMin, xMax));
-        positions.z.push(zMax - 0.5);
+        positions.x.push(getRandomInt(xMinAdjusted, xMaxAdjusted));
+        positions.z.push(zMaxAdjusted);
       }
     } else {
       if (Math.random() >= 0.5) {
-        positions.x.push(xMin + 2);
-        positions.z.push(getRandomInt(zMin, zMax));
+        positions.x.push(xMinAdjusted);
+        positions.z.push(getRandomInt(zMinAdjusted, zMaxAdjusted));
       } else {
-        positions.x.push(xMax - 2);
-        positions.z.push(getRandomInt(zMin, zMax));
+        positions.x.push(xMaxAdjusted);
+        positions.z.push(getRandomInt(zMinAdjusted, zMaxAdjusted));
       }
     }
   }
 
-  return positions;
-}
-
-function getRandomYPositions() {
-  const positions = [];
-  for (let i = 0; i < 100; i++) {
-    if (Math.random() >= 0.5) {
-      positions.push(getRandomInt(-10, -5));
-    } else {
-      positions.push(getRandomInt(5, 20));
-    }
-  }
   return positions;
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-/* eslint-disable prefer-const */
-/* eslint-disable no-restricted-syntax */
 import '@fortawesome/fontawesome-free/js/fontawesome';
-// import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/brands';
 import 'normalize.css';
 import './index.css';
 import { WEBGL } from 'three/examples/jsm/WebGL';
 
-import {
-  start,
-} from './control';
+import { start } from './control';
 
-const text = 'New insights can develop into new approaches. Our insights and achievements over the last year have allowed us to find new directions, to better our approach, and to fine tune our output.';
+const text =
+  'New insights can develop into new approaches. Our insights and achievements over the last year have allowed us to find new directions, to better our approach, and to fine tune our output.';
 
 function start3D() {
   if (WEBGL.isWebGLAvailable()) {
@@ -20,37 +16,32 @@ function start3D() {
     const warning = WEBGL.getWebGLErrorMessage();
     document.querySelector('#container').appendChild(warning);
   }
-  // create transition out once all letters clicked
 }
 
 function typingEffect(phrase, i) {
   // check if index is less than phrase length
-  if (i < (phrase.length)) {
-    document.querySelector('#description').innerHTML = `${phrase.substring(0, i + 1)}<span id="caret"></span>`;
+  if (i < phrase.length) {
+    document.querySelector('#description').innerHTML = `${phrase.substring(
+      0,
+      i + 1
+    )}<span id="caret"></span>`;
     setTimeout(() => {
       typingEffect(phrase, i + 1);
-    }, 50); // make this random between 50-100
+    }, 50); // TODO: make this random between 50-100
   }
-  // could create a code like scene?
-
-  return false;
 }
 
-export function fadeIn () {
-  let allHiddenComponents = document.querySelectorAll("#container .hide");
-  allHiddenComponents.forEach((component)=>{
-    component.classList.remove("hide");
+export function fadeIn() {
+  let allHiddenComponents = document.querySelectorAll('#container .hide');
+  allHiddenComponents.forEach(component => {
+    component.classList.remove('hide');
   });
 
   typingEffect(text, 0);
 }
 
 function init() {
-  // start game
   start3D();
-  // start typing effect
-  
-  // fade in elements
 }
 
 init();
